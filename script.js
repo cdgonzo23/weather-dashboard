bf1753afcd52c9299ecc4df13707babc800cfa30
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-05-24 19:17:34 -0500

    added fetch chain command and started appending

--- script.js
+++ script.js
@@ -1,19 +1,107 @@
-var currentWeather = document.querySelector('#city-current-weather');
-var forecast = document.querySelector('#forecast');
-var prevSearches = document.querySelector('#prev-searches')
+var currentWeather = document.querySelector('#current-weather-container');
+var forecastContainer = document.querySelector('#forecast-container');
+var prevSearches = document.querySelector('#recent-searches');
+var cityForm = document.querySelector('#city-form');
+var citySearch = document.querySelector('#city-search');
+var cityName = document.querySelector('#city-search-name');
+var todaysDate = dayjs().format('MM/DD/YYYY');
+var apiKey = "2493754d1fbba673aa3e32978ca44e41";
 
-function searchWeather(weather, format) {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}')
-    // what results do we need from the weatherAPI? 5-day forecast, temp, wind, humidity
-        .then(function (response) {
+
+function handleSearchForm(event) {
+    event.preventDefault();
+
+    var city = citySearch.value.trim()
+
+    if (city) {
+        searchCity(city);
+
+        citySearch.value = '';
+    }
+}
+
+
+
+function searchCity(city) {
+    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
+
+    fetch(geoUrl)
+        .then(function(response) {
             return response.json();
         })
-        .then(function (data) {
-            for (var result of data.results) {
-                // need to create cards for each of the five days as well as a card to display current weather and date
+        .then(function(data) {
+            console.log(data);
+            for (var result of data) {
+                console.log(result.lat);
+                console.log(result.lon);
+                var lat = result.lat
+                var lon = result.lon
+                getCurrentWeather(lat, lon)
+                getWeatherForecast(lat, lon)
             }
         })
+        .catch(function(error) {
+            console.log('request failed', error)
+        })
 }
+
+function getCurrentWeather(lat, lon) {
+    var currWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
+    fetch(currWeatherUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            // console.log(data.main.temp)
+            currentWeather.innerHTML = null;
+            cityName.textContent = citySearch.value;
+            if (data.length === 0) {
+                cityName.textContent = 'No City Found';
+                return;
+            }
+            for (var result of data.weather) {
+                console.log(result.main.temp)
+                // var weatherEl = document.createElement('div');
+                // var weatherIcon = result.weather.icon;
+                // var countryId = result.sys.country
+                // weatherEl.classList = 'flex-row justify-content-center align-center';
+                // var weatherTitle = document.createElement('h4');
+                // weatherTitle.textContent = citySearch.value + ", " + countryId + ' (' + todaysDate + ") " + weatherIcon;
+                // var conditionsListEl = document.createElement('ul');
+                // var temperature = document.createElement('li');
+                // var windSpeed = document.createElement('li');
+                // var humidity = document.createElement('li');
+                // temperature.textContent = result.main.temp;
+                // windSpeed.textContent = result.wind.speed;
+                // humidity.textContent = result.main.humidity;
+                // conditionsListEl.appendChild(temperature, windSpeed, humidity)
+                // weatherEl.appendChild(weatherTitle, conditionsListEl);
+                // currentWeather.appendChild(weatherEl);
+
+            }
+
+        })
+        // .catch(function(error) {
+        //     console.log('request failed', error)
+        // })
+}
+
+function getWeatherForecast(lat, lon) {
+    var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+    fetch(forecastUrl)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+        .catch(function(error) {
+            console.log('request failed', error)
+        })
+}
+
+cityForm.addEventListener("submit", handleSearchForm);
 
 // need to create a space for the previous searched cities (will be underneath the search button)
 
